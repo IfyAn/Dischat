d547c540612861e3599aa9fb918dbde1970dd9c1
IfyAn <chukwuanyan@gmail.com>
2022-07-22 05:30:19 -0700

    first commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from './src/hooks/useCachedResources';
+import useColorScheme from './src/hooks/useColorScheme';
+import Navigation from './src/navigation';
+import { StreamChat } from "stream-chat";
 
+
+const API_KEY = "c4q86rjugvmx";
+const client = StreamChat.getInstance(API_KEY);
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
